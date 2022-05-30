dba6bbe94b782634e17ce77e04781b410297fc7d
gumicode <gumicode@gmail.com>
2022-05-30 22:41:28 +0900

    first commit

---
+++ src/main/java/com/example/awsjavasdkexample/AwsJavaSdkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsjavasdkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJavaSdkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsJavaSdkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsjavasdkexample/AwsJavaSdkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsjavasdkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJavaSdkExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
